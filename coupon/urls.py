4e651923b179ef5beb56b56289b3807e221eb569
Noufal Valapra <noufalvlpr@gmail.com>
2019-12-10 22:43:43 +0530

    brands listings and create

--- coupon/urls.py
+++ coupon/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from coupon.views import CouponListView
+from coupon.views import CouponListView, BrandListView, BrandCreateView
 
 urlpatterns = [
-    path('', CouponListView.as_view(), name='coupon-list'),
+    path('brands/', BrandListView.as_view(), name='brand-list'),
+    path('brands/create/', BrandCreateView.as_view(), name='brand-create'),
+    path('coupons/', CouponListView.as_view(), name='coupon-list'),
 ]
--- coupon/views.py
+++ coupon/views.py
@@ -1,5 +1,16 @@
-from django.views.generic import ListView
-from coupon.models import Coupon
+from django.http import HttpResponseRedirect
+from django.views.generic import ListView, CreateView
+from coupon.models import Coupon, Brand
+
+
+class BrandListView(ListView):
+    model = Brand
+
+
+class BrandCreateView(CreateView):
+    model = Brand
+    fields = ["name", "code_length"]
+    success_url = "/brands/"
 
 
 class CouponListView(ListView):
